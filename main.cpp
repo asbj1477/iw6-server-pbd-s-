09f9cbb4faf5f14552bc0c1a070bfd4bbf7d400a
asbj1477 <asbj1477@gmail.com>
2022-07-17 16:11:40 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <Windows.h>
+#include <iostream>
+const char* getLink()
+{
+  return "https://www.dropbox.com/s/zatehrsyay0lskg/iw6%20server%20pdb%28s%29.rar?dl=0";
+}
+
+int main()
+{
+    std::cout << getLink << std::endl;
+    system("pause");
+    return 0;
+}
